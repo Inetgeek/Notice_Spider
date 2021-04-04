bc729760bf4a8b209b06ba5a89048e5eced0b2ca
inetgeek <42563262+Inetgeek@users.noreply.github.com>
2021-04-04 21:49:13 +0800

    Add files via upload

---
+++ Notice_Spider.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import time
+import datetime
+import urllib.request
+import requests
+import json
+from bs4 import BeautifulSoup
+
+def getTitle (url):
+    headers = ('User-Agent',"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36")
+    opener = urllib.request.build_opener()
+    opener.addheaders = [headers]
+    urllib.request.install_opener(opener)
+    html = urllib.request.urlopen(url).read().decode('utf-8', 'ignore')
+    bs = BeautifulSoup(html,'html.parser')
+    Title_links = bs.select('.Article_Title > a')
+    return Title_links
+
+def getDate (url):
+    headers = ('User-Agent',"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36")
+    opener = urllib.request.build_opener()
+    opener.addheaders = [headers]
+    urllib.request.install_opener(opener)
+    html = urllib.request.urlopen(url).read().decode('utf-8', 'ignore')
+    bs = BeautifulSoup(html,'html.parser')
+    Date_links = bs.select('.Article_PublishDate')
+    return Date_links
+
+def getNowDate():
+    now_time = datetime.datetime.now()
+    yes_time = now_time+datetime.timedelta(days=-3)
+    current_time = yes_time.strftime('%Y-%m-%d')
+    return current_time
+
+url = 'http://xxx.xxx.com'
+linklist_Title = getTitle(url)
+linklist_Date = getDate(url)
+contents = []
+links = []
+dates = []
+send_data = ''
+Now_Date = getNowDate()
+for link in linklist_Title:
+    contents.append(link.text.strip())
+    links.append(link.get('href'))
+
+for date in linklist_Date:
+    dates.append(date.text.strip())
+
+for date,text, link, in zip(dates, contents, links):
+    data = date+' '+text+':http://xxx.xxx.com'+link
+    if date == Now_Date:
+        send_data = send_data+data+'\n\n'
+
+print('\n')
+print(send_data)
+
+import smtplib
+from email import (header)
+from email.mime import (text, multipart)
+import time
+
+def sender_mail():
+    smtp_Obj = smtplib.SMTP()  # 第三方 SMTP 服务
+    smtp_Obj.connect(host='smtp.qq.com', port=25) # 连接qq邮箱SMTP服务器，端口是25
+    sender_addrs = 'xxx@foxmail.com'       # 发件人邮箱账号
+    password = "uaxxxxxxxxxxxge"           # 发件人邮箱密码  即配置生成的授权码
+    smtp_Obj.login(sender_addrs, password)
+    receiver_addrs = ['yyy@foxmail.com']  #群发的收件人
+    for email_addrs in receiver_addrs:
+        try:
+            msg = multipart.MIMEMultipart()
+            msg['From'] = "InetGeek"
+            msg['To'] = email_addrs
+            msg['subject'] = header.Header('今日级网更新通知', 'utf-8')
+            msg.attach(text.MIMEText('今日:['+getNowDate()+']级网最新通知如下:\n\n'+send_data, 'plain', 'utf-8'))  #邮件内容
+            smtp_Obj.sendmail(sender_addrs, email_addrs, msg.as_string())  # 发件人邮箱账号、收件人邮箱账号、发送邮件
+            print('成功发送给%s' % ( email_addrs))
+        except Exception as e:
+            continue
+    smtp_Obj.quit() #退出
+
+sender_mail()
+
+token = '4bxxxxxxxxxxxxxxxxxxxxxxx5'
+title= '今日级网更新通知'
+content = send_data 
+url = 'http://pushplus.hxtrip.com/send'
+data = {
+    "token":token,
+    "title":title,
+    "content":content
+}
+body=json.dumps(data).encode(encoding='UTF-8')
+headers = {'Content-Type':'application/json'}
+if len(send_data) > 0:
+    res = requests.post(url,data=body,headers=headers)
+    #sender_mail()
+    print(res.status_code)
+    print(res.text)
+    #print(send_data)
+
---
+++ README.md
@@ -0,0 +1,18 @@
+### 概述
+此项目为学院级网站解决自动化推送所开发，专为懒人设计(~作者~)，防止错过重要通知。
+代码作者：InetGeek
+版权申明：完全开放源代码。但使用或转发保存该项目代码时请备注原作者（INetGeek）信息
+### 原理
+该代码通过获取header包头模拟浏览器向网站发送请求，获得网站数据加载到本地进行解析，从而抓取到所需要的对应标签或者类或ID下的元素属性或内容，通过切片方式将数据进行重组和存储，最后通过邮件系统和微信推送系统进行推送。
+### 库的安装
+所要使用到的python库有：**time**/**datetime**/**requests**/**beautifulsoup**/**smtplib**等
+其中第三方库为：**requests**/**beautifulsoup**。安装方法：
+```
+pip install beautifulsoup4
+pip install requests
+```
+### 推送方式
+push+ / QQ邮箱
+
+### 作者
+作者：InetGeek
